a9cebcb019a82d6e04aa41bcefd7ac3adab3fecc
Dan Carroll <dancarroll@gmail.com>
2024-12-27 02:05:04 -0500

    Fix comment

--- lib/clique.dart
+++ lib/clique.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Dart support for finding cliques in an undirected graph.
 library;
 
 export 'src/bron_kerbosch.dart';
-
-// TODO: Export any libraries intended for clients of this package.
